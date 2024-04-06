7426b9c8e9fcc2d825ff17dee9e508fa4769c54c
Margvela17 <margvela2006>
2024-04-06 12:12:28 +0400

    init

---
+++ app/components/header.jsx
@@ -0,0 +1,17 @@
+export default function Header(){
+    
+    return(
+        <div className="flex justify-between bg-white text-2xl pt-8 h-24">
+        <h1 className="flex justify-start text-4xl text-black pl-24">Phuler</h1>
+        
+        <header className="flex justify-end">
+          
+          <a className="text-black text-xl mr-12 hover:text-red-600">Home</a>
+          <a className="text-black text-xl mr-12 hover:text-red-600">About</a>
+          <a className="text-black text-xl mr-12 hover:text-red-600">Shop</a>
+          <a className="text-black text-xl mr-12 hover:text-red-600">Blog</a>
+          <a className="text-black text-xl mr-12 hover:text-red-600">Pages</a>
+          <a className="text-black text-xl mr-12 hover:text-red-600">Contact</a>
+        </header></div>
+    )
+};
---
+++ app/components/main.jsx
@@ -0,0 +1,25 @@
+export default function Main(){
+
+    return(
+        <main className="flex flex-col  justify-between p-24" 
+    style={{
+      backgroundImage:`url('/bcgr.jpg')`,
+      height:"100vh"
+      }} >
+        <div>
+      <h2 className="text-red-600 text-5xl mr-9 mt-32 font-serif ">
+        New Arrivals
+      </h2>
+      <h1 className="text-gray-900 text-6xl font-serif ml-1 mt-4">
+        For Mother's Day!
+      </h1>
+      <h3 className="text-black mt-5 font-normal text-l">
+        Exclusive Offer -10% Off This Week
+      </h3>
+      <button className="bg-red-700 text-sm mt-9 border-red-700 border-radius rounded-md border-8 ml-4 hover:bg-white hover:border-white hover:text-red-700 font-medium w-32 h-12">
+        Shop Now
+      </button>
+    </div>
+    </main>
+    )
+}
